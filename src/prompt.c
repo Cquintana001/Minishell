d6c885f8cd306a50d6c4eacf5b27bac7244d4b55
Cquintana001 <cquintana002@gmail.com>
2022-09-20 08:37:22 +0200

    check command function working

--- src/prompt.c
+++ src/prompt.c
@@ -12,12 +12,29 @@ char *find_path(char **envp)
     while(*envp)
         {
             if(ft_strnstr(*envp, "PATH=", 5))
-                return(*envp);
+                return(ft_strchr(*envp, '/'));
             envp++;
         }
     return(0);
 }
+int check_if_command(char **envp, char *str)
+{
+    char **path_list;
+    char *cmd;
+    char *full_cmd_path;
 
+    cmd = ft_strjoin("/", str);
+    path_list = ft_split(find_path(envp), ':');
+    while(*path_list)
+    {
+        full_cmd_path = ft_strjoin(*path_list, cmd);
+        if(access(full_cmd_path, F_OK) == 0)
+            return(0);
+        path_list++;
+    }
+    return(1);
+
+}
 int main(int argc, char *argv[], char **envp)
 {
     argc = 0;
@@ -30,17 +47,17 @@ int main(int argc, char *argv[], char **envp)
         char **split;
         x = array(str);
         split = ft_split(x, ' ');
-        char *path = ft_strjoin("/bin/", split[0]);
+/*         char *path = ft_strjoin("/bin/", split[0]);
         int ok = access(path, F_OK);
-        path = ft_strchr(find_path(envp), '/');
-
-        char **array = ft_split(path, ':');
+        path = find_path(envp);
+        char **array = ft_split(path, ':'); */
+        int ok = check_if_command(envp, split[0]);
         printf("el archivo da %d\n", ok);
-        while(*array)
+        /* while(*array)
         {
         printf("el path es: %s\n", *array);
         array++;
-        }
+        } */
          
                 
          
